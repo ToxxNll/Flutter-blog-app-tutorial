496a9efe97eeabdee6cbdb8f3b0f98a405c42025
ToxxNll <dolayevtohtar@gmail.com>
2026-09-05 19:35:27 +0500

    Adding + utils (reusable snackBar method) + custom loader

--- app/lib/features/auth/presentation/pages/signup_page.dart
+++ app/lib/features/auth/presentation/pages/signup_page.dart
@@ -1,4 +1,6 @@
+import 'package:app/core/common/widgets/loader.dart';
 import 'package:app/core/theme/app_palette.dart';
+import 'package:app/core/utils/show_snackbar.dart';
 import 'package:app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:app/features/auth/presentation/widgets/auth_field.dart';
 import 'package:app/features/auth/presentation/widgets/auth_gradient_button.dart';
@@ -31,79 +33,102 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     // formKey.currentState!.validate();
-    return BlocListener<AuthBloc, AuthState>(
-      listener: (context, state) {
-        if (state case AuthFailure(:final message)) {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(message)));
-        } else if (state is AuthSuccess) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Text('Account created. Check your email to confirm it.'),
-            ),
-          );
-        }
-      },
-      child: Scaffold(
-        appBar: AppBar(),
-        body: Padding(
-          padding: EdgeInsets.all(15.0),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 30),
-                AuthField(hintText: 'Name', controller: nameController),
-                const SizedBox(height: 15),
-                AuthField(hintText: 'Email', controller: emailController),
-                const SizedBox(height: 15),
-                AuthField(
-                  hintText: 'Password',
-                  controller: passwordController,
-                  isPassword: true,
-                ),
-                const SizedBox(height: 20),
-                AuthGradientButton(
-                  buttonText: "Sign Up",
-                  onPressed: () {
-                    if (formKey.currentState!.validate()) {
-                      context.read<AuthBloc>().add(
-                        AuthSignUp(
-                          email: emailController.text.trim(),
-                          name: nameController.text.trim(),
-                          password: passwordController.text.trim(),
-                        ),
-                      );
-                    }
-                  },
-                ),
-                const SizedBox(height: 20),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.maybePop(context);
-                  },
-                  child: RichText(
-                    text: TextSpan(
-                      text: 'Already have an account? ',
-                      style: Theme.of(context).textTheme.titleMedium,
-                      children: [
-                        TextSpan(
-                          text: 'Sign In',
-                          style: Theme.of(context).textTheme.titleMedium
-                              ?.copyWith(color: AppPallete.gradient2),
-                        ),
-                      ],
-                    ),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: BlocConsumer<AuthBloc, AuthState>(
+          listener: (context, state) {
+            if (state is AuthFailure) {
+              showSnackBar(context, state.message);
+            } else if (state is AuthSuccess) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text(
+                    'Account created. Check your email to confirm it.',
                   ),
                 ),
-              ],
-            ),
-          ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is AuthLoading) {
+              return const Loader();
+            }
+            
+            return Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  AuthField(
+                    hintText: 'Name',
+                    controller: nameController,
+                  ),
+                  const SizedBox(height: 15),
+                  AuthField(
+                    hintText: 'Email',
+                    controller: emailController,
+                  ),
+                  const SizedBox(height: 15),
+                  AuthField(
+                    hintText: 'Password',
+                    controller: passwordController,
+                    isPassword: true,
+                  ),
+                  const SizedBox(height: 20),
+                  AuthGradientButton(
+                    buttonText: "Sign Up",
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        context.read<AuthBloc>().add(
+                          AuthSignUp(
+                            email: emailController.text.trim(),
+                            name: nameController.text.trim(),
+                            password: passwordController.text
+                                .trim(),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.maybePop(context);
+                    },
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Already have an account? ',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium,
+                        children: [
+                          TextSpan(
+                            text: 'Sign In',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium
+                                ?.copyWith(
+                                  color: AppPallete.gradient2,
+                                ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
