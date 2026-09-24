8101b812fee3c28c78a2ed2cf3b946ab5ab2b851
ToxxNll <dolayevtohtar@gmail.com>
2026-09-24 21:16:29 +0500

    added internet connection checker and implemented to Auth

--- app/lib/features/auth/data/repositories/auth_repository_impl.dart
+++ app/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,20 +1,40 @@
 import 'package:app/core/error/exceptions.dart';
 import 'package:app/core/error/failures.dart';
+import 'package:app/core/network/connection_checker.dart';
 import 'package:app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:app/core/entities/user.dart';
+import 'package:app/features/auth/data/models/user_model.dart';
 import 'package:app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' as sb;
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource authRemoteDataSource;
-  const AuthRepositoryImpl(this.authRemoteDataSource);
+  final ConnectionChecker connectionChecker;
+  const AuthRepositoryImpl(
+    this.authRemoteDataSource,
+    this.connectionChecker,
+  );
 
   @override
   Future<Either<Failure, User>> currentUser() async {
     try {
-      final user = await authRemoteDataSource
-          .getCurrentUserData();
+      if (!await (connectionChecker.isConnected)) {
+        final session = authRemoteDataSource.currentUserSession;
+
+        if (session == null) {
+          return left(Failure('User not logged in'));
+        }
+
+        return right(
+          UserModel(
+            id: session.user.id,
+            email: session.user.email ?? '',
+            name: '',
+          ),
+        );
+      }
+      final user = await authRemoteDataSource.getCurrentUserData();
       if (user == null) {
         return left(Failure('User not logged in'));
       }
@@ -58,6 +78,10 @@ class AuthRepositoryImpl implements AuthRepository {
     Future<User> Function() fn,
   ) async {
     try {
+      if (!await (connectionChecker.isConnected)) {
+        return left(Failure('No internet connection!'));
+      }
+
       final user = await fn();
 
       return right(user);
--- app/lib/init_dependencies.dart
+++ app/lib/init_dependencies.dart
@@ -1,4 +1,5 @@
 import 'package:app/core/common/cubits/app_user/app_user_cubit.dart';
+import 'package:app/core/network/connection_checker.dart';
 import 'package:app/core/secrets/app_secrets.dart';
 import 'package:app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:app/features/auth/data/repositories/auth_repository_impl.dart';
@@ -14,6 +15,7 @@ import 'package:app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final serviceLocator = GetIt.instance;
@@ -27,8 +29,13 @@ Future<void> initDependencies() async {
   );
   serviceLocator.registerLazySingleton(() => supabase.client);
 
+  serviceLocator.registerLazySingleton(() => InternetConnection());
+
   //core
   serviceLocator.registerLazySingleton(() => AppUserCubit());
+  serviceLocator.registerLazySingleton<ConnectionChecker>(
+    () => ConnectionCheckerImpl(serviceLocator()),
+  );
 }
 
 void _initAuth() {
@@ -39,7 +46,7 @@ void _initAuth() {
     )
     // Repository
     ..registerFactory<AuthRepository>(
-      () => AuthRepositoryImpl(serviceLocator()),
+      () => AuthRepositoryImpl(serviceLocator(), serviceLocator()),
     )
     // Usecases
     ..registerFactory(() => UserSignUp(serviceLocator()))
